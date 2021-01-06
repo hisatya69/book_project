6fb2abef591474d141a663a2192e3ab2d8a10b95
Anastasija Radonjic <anastasijaradonjic99@gmail.com>
2021-01-06 19:52:47 +0100

    Dodavanje funkcija za stranicu(proba).

--- library/urls.py
+++ library/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='library_index'),
-    path('int/<int:br>', views.broj, name='library_broj')
+    path('int/<int:br>', views.broj, name='library_broj'),
+    path('int/', views.broj, name='library_broj_def'),
+    path('rec/<str:rec>', views.rec, name='library_rec'),
+    path('params/', views.params, name='library_params')
 ]
--- library/views.py
+++ library/views.py
@@ -5,5 +5,17 @@ def index(request):
     return HttpResponse('Zdravo !')
 
 
-def broj(request, br):
+def broj(request, br=0):
     return HttpResponse('Broj: '+ str(br))
+
+
+def rec(request, rec):
+    return HttpResponse('Rec je: '+ rec)
+
+
+def params(request):
+    return HttpResponse('Params: '+str([str(k)+': '+str(v) for k, v in request.GET.items()]))
+
+
+def regex(request, mesec, godina):
+    return HttpResponse('Godina: '+ godina + ', Mesec'+ mesec)
